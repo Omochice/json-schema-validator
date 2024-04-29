1642508d13a6a7b27e38347ee567d4d0d94b888f
Omochice <mochice.mls.ntl@gmail.com>
2024-04-30 00:36:35 +0900

    feat: run validate parallel

--- cmd/json-schema-validator/main.go
+++ cmd/json-schema-validator/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"path/filepath"
 
@@ -11,44 +13,55 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func run(filename string, ignoreNonSchema bool, wg *sync.WaitGroup, errCh chan error) {
+	defer wg.Done()
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		log.Println(err)
+		errCh <- err
+		return
+	}
+	if err := validator.ValidateJSONSchema(path, ignoreNonSchema); err != nil {
+		log.Println(err)
+		errCh <- err
+	}
+}
+
 func main() {
-	ignoreNonSchema := false
 	app := &cli.App{
 		Name:  "json-schema-validator",
 		Usage: "validate json file by its $schema field",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
-				Name:        "quiet-on-non-schema",
-				Usage:       "Be quiet error logging files without $schema field",
-				Destination: &ignoreNonSchema,
-				Aliases:     []string{"q"},
+				Name:    "quiet-on-non-schema",
+				Usage:   "Be quiet error logging files without $schema field",
+				Aliases: []string{"q"},
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			hasError := false
+			errCh := make(chan error, cCtx.NArg())
+			var wg sync.WaitGroup
 			for i := 0; i < cCtx.NArg(); i++ {
-				f := cCtx.Args().Get(i)
-				prefix := fmt.Sprintf("%s: ", f)
-				path, err := filepath.Abs(f)
-				if err != nil {
-					log.Println(prefix + err.Error())
-					hasError = true
-					continue
-				}
-				if err = validator.ValidateJSONSchema(path, ignoreNonSchema); err != nil {
-					log.Println(prefix + err.Error())
-					continue
-				}
-				log.Println(prefix + "ok")
+				wg.Add(1)
+				go run(cCtx.Args().Get(i), cCtx.Bool("quiet-on-non-schema"), &wg, errCh)
 			}
-			if hasError {
-				return fmt.Errorf("Some files are not valid")
+			wg.Wait()
+			go func() {
+				for err := range errCh {
+					fmt.Fprintln(os.Stderr, err)
+				}
+			}()
+
+			close(errCh)
+
+			if len(errCh) > 0 {
+				return errors.New("error occurred")
 			}
 			return nil
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
 }
