c28dc2c51170dc8efedc16d33a22067a7348b14c
Omochice <44566328+Omochice@users.noreply.github.com>
2024-08-19 00:49:45 +0900

    feat: run validate parallel (#29)
    * feat: run validate parallel
    
    * fixup! feat: run validate parallel
    
    * refactor: use errgroup instead
    
    * refactor: use multierror instead
    
    * chore: add check task
    
    * refactor: use errors.new instead

--- cmd/json-schema-validator/main.go
+++ cmd/json-schema-validator/main.go
@@ -1,48 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"path/filepath"
-
 	validator "github.com/Omochice/json-schema-validator"
+	multierror "github.com/hashicorp/go-multierror"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	ignoreNonSchema := false
 	app := &cli.App{
 		Name:  "json-schema-validator",
 		Usage: "validate json file by its $schema field",
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
-				Name:        "quiet-on-non-schema",
-				Usage:       "Be quiet error logging files without $schema field",
-				Destination: &ignoreNonSchema,
-				Aliases:     []string{"q"},
+				Name:    "quiet-on-non-schema",
+				Usage:   "Be quiet error logging files without $schema field",
+				Aliases: []string{"q"},
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			hasError := false
+			var meg multierror.Group
+			ignoreNonSchema := cCtx.Bool("quiet-on-non-schema")
 			for i := 0; i < cCtx.NArg(); i++ {
-				f := cCtx.Args().Get(i)
-				prefix := fmt.Sprintf("%s: ", f)
-				path, err := filepath.Abs(f)
-				if err != nil {
-					log.Println(prefix + err.Error())
-					hasError = true
-					continue
-				}
-				if err = validator.ValidateJSONSchema(path, ignoreNonSchema); err != nil {
-					log.Println(prefix + err.Error())
-					continue
-				}
-				log.Println(prefix + "ok")
+				meg.Go(func() error {
+					return validator.ValidateJSONSchema(cCtx.Args().Get(i), ignoreNonSchema)
+				})
 			}
-			if hasError {
-				return fmt.Errorf("Some files are not valid")
+
+			merr := meg.Wait()
+			if merr != nil {
+				return merr
 			}
 			return nil
 		},
